5688fabea01c6fc5720cdd81da9711c9d505b77e
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-09-11 15:37:12 +0200

    added html and css

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            randomMail: '',
 
         }
-    }
+    },
+
+    methods: {
+        getRandomEmail() {
+            axios.get(this.apiUrl)
+                .then((response) => {
+                    this.randomMail = response.data.response;
+                    console.log(response.data.response);
+                });
+        }
+    },
+
+    mounted() {
+        this.getRandomEmail()
+    },
+
 }).mount('#app');
+
